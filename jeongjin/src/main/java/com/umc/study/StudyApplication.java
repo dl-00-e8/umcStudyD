45a7a09b8d164a9c7264505fd7dfbfdc0e139813
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-21 01:50:17 +0900

    Chore: BaseTimeEntity 설정

--- jeongjin/src/main/java/com/umc/study/StudyApplication.java
+++ jeongjin/src/main/java/com/umc/study/StudyApplication.java
@@ -2,7 +2,9 @@ package com.umc.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class StudyApplication {
 
